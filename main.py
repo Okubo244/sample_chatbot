8d8a216f8dc6af73b7716179921a9c5a4a7792fd
Okubo244 <66934563+Okubo244@users.noreply.github.com>
2022-01-21 03:29:26 +0900

    Update main.py

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 
 def main():
     USER_ID = info['USER_ID']
-    messages = TextSendMessage(text = "こんにちは！ \n 本日のランキングサイトです。")
-    messages = TextSendMessage(text = "url")
+    messages = TextSendMessage(text = "こんにちは！ \n 本日のランキングサイトです。"),
+    messages = TextSendMessage(text = "https://scraping-for-beginner.herokuapp.com/ranking/"),
     line_bot_api.push_message(USER_ID, messages = messages)
     
 if __name__ == "__main__":
